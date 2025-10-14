2cd95f7da12d9386b5e2e66e97dca252c4691bef
Dj <148027370+djsy104@users.noreply.github.com>
2025-10-14 14:35:34 -0700

    fix: fixed import statements in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import './index.css';
 import App from './App.jsx';
-import ThemeProvider from './shared/ThemeProvider.jsx';
+import ThemeProvider from './shared/Theme/ThemeProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

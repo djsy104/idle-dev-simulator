53c20474aaea1e96bfe12cb9835110aa8e80bf07
Dj <148027370+djsy104@users.noreply.github.com>
2025-10-20 21:25:31 -0700

    feat: Setup scaffold structure and theme change
    - Added basic grid and flexbox styling
    - Added functionality for theme changes

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,21 +1,75 @@
-import { NavLink, Link } from 'react-router';
+import useTheme from '../shared/Theme/useTheme';
 import NavMenu from '../shared/NavigationMenu/NavMenu';
+import useSound from '../shared/Sounds/useSound';
 import styles from './Settings.module.css';
 
 function Settings() {
+  const { theme, setTheme } = useTheme();
+
+  function handleThemeChange(event) {
+    setTheme(event.target.value);
+  }
+
   return (
     <div className={styles.settingsContainer}>
       <NavMenu />
       <div className={styles.header}>
         <h4 className={styles.title}>Settings</h4>
         <p>Placeholder text</p>
+        <hr />
       </div>
       <aside className={styles.sidebar}>
-        <a href="">Display</a>
-        <a href="">Audio</a>
-        <a href="">Gameplay</a>
+        <a href="#display" className={styles.navigationLink}>
+          Display
+        </a>
+        <a href="#audio" className={styles.navigationLink}>
+          Audio
+        </a>
+        <a href="#gameplay" className={styles.navigationLink}>
+          Gameplay
+        </a>
       </aside>
-      <main className={styles.main}></main>
+      <main className={styles.mainContainer}>
+        <section id="display" className={styles.displayContainer}>
+          <h5>Display</h5>
+          <p>Adjust the color of the interface for better visibility.</p>
+          <hr />
+          <form>
+            <input
+              type="radio"
+              id="lightMode"
+              name="theme"
+              checked={theme === 'light'}
+              value="light"
+              onChange={(e) => handleThemeChange(e)}
+            />
+            <label for="html">Light</label>
+            <br />
+            <input
+              type="radio"
+              id="darkMode"
+              name="theme"
+              checked={theme === 'dark'}
+              value="dark"
+              onChange={(e) => handleThemeChange(e)}
+            />
+            <label for="css">Dark</label>
+            <br />
+          </form>
+        </section>
+
+        <section id="audio" className={styles.audioContainer}>
+          <h5>Display</h5>
+          <p>Adjust the color of the interface for better visibility.</p>
+          <hr />
+        </section>
+
+        <section id="gameplay" className={styles.audioContainer}>
+          <h5>Display</h5>
+          <p>Adjust the color of the interface for better visibility.</p>
+          <hr />
+        </section>
+      </main>
     </div>
   );
 }
